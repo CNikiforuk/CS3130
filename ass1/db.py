338b965feb810641a41c3ec718bb797a6044ffd9
Carlos Nikiforuk <CarNikiforuk@gmail.com>
2015-01-22 07:04:32 -0700

    need more exceptions

--- ass1/db.py
+++ ass1/db.py
@@ -1,23 +1,34 @@
+
+################--Description--#################
+#Basic employee database functions file.
+
+################-----Author-----#################
+#Carlos Nikiforuk
+
+
 ID, FIRST, LAST, DEPT = range(4)
 MAXNAMESIZE = 16
 
+##################################
 class database:
+#Database class that is instantiated and associated with the infile
+#Support could be added for operations between distinct databases
+##################################
+
     def __init__(self, infile):
         self.infile = infile
 
-#    def load(self, infile):
- #       with open(infile, 'r') as file:
- #           open(infile, 'x')
-  #      with open(infile, 'r+') as tmp:  
-  #          tmp.writelines(file)
-  #      file.close()
-  #      return tmp
-
+    ##################################
     def addEmployee(self, line):
+    #Add Employee to database, expects complete string passed
+    ##################################
          with open(self.infile, 'a') as file:
             file.write(line+"\n")
-        
+    
+    ##################################    
     def findEmployee(self, id):
+    #Find Employee in database. ID passed in, fields returned
+    ##################################
         with open(self.infile, 'r') as file:
                 for line in file:
                     line = line.rstrip()
@@ -28,8 +39,10 @@ class database:
                             return fields  
         return 0
         
-
+    ##################################
     def removeEmployee(self, f):
+    #Remove employee from database, fields passed
+    ##################################
         new = []
         file = open(self.infile, 'r')
         for line in file:
@@ -42,8 +55,10 @@ class database:
             file.writelines(new)
             file.close()
                 
-
+    ##################################
     def showEmployees(self):
+    #Print whole database, formatted
+    ##################################
         print("{0:<4} {1:<{n}} {2:<15}".format("ID", "Name", "Dept", n=MAXNAMESIZE*2))
         print("{0:-<4} {0:-<{n}} {0:-<15}".format("", n=MAXNAMESIZE*2))
         with open(self.infile, 'r') as file:
@@ -53,5 +68,8 @@ class database:
                         fields = line.split(":")
                         self.printEmployee(fields) 
 
+    ##################################    
     def printEmployee(self, fields):
+    #Print an Employee line, Employee field passed
+    ##################################
         print("{0:<4} {1:<{n}} {2:<{n}} {3:<15}".format(fields[ID], fields[FIRST], fields[LAST], fields[DEPT], n=MAXNAMESIZE))
--- ass1/main.py
+++ ass1/main.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 import db
 
-#Author: Carlos Nikiforuk
-#Description: Basic employee database stored as txt file.
+################--Description--#################
+#Basic employee database example main run file.
+
+################-----Author-----#################
+#Carlos Nikiforuk
 
 #IDEA FOR FUNCTIONIZING, PRINT WHATEVER IS RETURNED. TRY/EXCEPTS, OR SUCCESS MESSAGE, OR FAILURE
 
-infile = 'db.txt'
 ID, FIRST, LAST, DEPT = range(4)
 MAXNAMESIZE = 16
 
 def main():
 
     if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:
-        print("usage: {0} file".format(sys.argv[0]))
-
+        print("usage: {0} <db file>".format(sys.argv[0]))
+        exit()
+    
+    infile = sys.argv[1]
 
     db1 = db.database(infile)
     cont = True
     
     while(cont):
     
-        print("\nEmployee FMS\n")
+        print("\n-----------Employee FMS-----------\n")
         print("Select one of the following:\n")
         print("    1) Add a new employee")
-        print("    2) Search for a employee")   
+        print("    2) Search for an employee")   
         print("    3) Remove an employee from FMS")
         print("    4) Display the entire employee FMS")
         print("    5) Quit")
@@ -44,35 +47,41 @@ def main():
             f = db1.findEmployee(fields[ID])
             try:
                 if(MAXNAMESIZE < max([len(fields[FIRST]), len(fields[LAST]), len(fields[DEPT])])):
-                    print("    Name or dept too long!")
+                    print("Name or dept too long!")
                 elif(f == 0 and len(fields[ID]) == 4):
                     db1.addEmployee(line)
-                    print("    Employee ",fields[ID]," Added!")
+                    print("Employee ",fields[ID]," Added!")
                 else:
-                    print("    Error: Bad ID or duplicate entry")
+                    print("Error: Bad ID or duplicate entry")
             except IndexError:
-                print("    Error: Incomplete string!")
+                print("Error: Incomplete string!")
             except Exception as e:
-                print("    Error: ",e)
+                print("Error: ",e)
 
         elif(a=='2'):   #search for employee
             id = input("Enter employee ID to find: ")
             f = db1.findEmployee(id)
             if(f == 0):
-                print("    Not found!")
+                print("\nNot found!")
             else:
                 print("{0:<4} {1:<{n}} {2:<15}".format("ID", "Name", "Dept", n=MAXNAMESIZE*2))
                 print("{0:-<4} {0:-<{n}} {0:-<15}".format("", n=MAXNAMESIZE*2))
                 db1.printEmployee(f)
             
         elif(a=='3'):   #remove employee
-            id = input("Enter employee ID to remove: ")
+            id = input("\nEnter employee ID to remove: ")
             f = db1.findEmployee(id)
             if(f == 0):
-                print("    Employee not found!")
+                print("\nEmployee not found!")
             else:
-                db1.removeEmployee(f)
-                print("    Employee ",id," removed!")
+                print("Employee found: ")
+                print("{0:<4} {1:<{n}} {2:<15}".format("ID", "Name", "Dept", n=MAXNAMESIZE*2))
+                print("{0:-<4} {0:-<{n}} {0:-<15}".format("", n=MAXNAMESIZE*2))
+                db1.printEmployee(f)
+                verify = input("\nAre you sure you with to delete this employee (y/n)? " )
+                if(verify == 'y'):
+                    db1.removeEmployee(f)
+                    print("\nEmployee ",id," removed!")
 
         elif(a=='4'):   #show employees
             db1.showEmployees()
@@ -81,10 +90,7 @@ def main():
             cont = False
         
         if(a != '5'):
-            input("\nPress any key to continue")
-
-
-    #os.remove("~db.txt")
+            input("\nPress enter to continue...")
 
 
 main()
