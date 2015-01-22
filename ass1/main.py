8ef4a82778e779e89a517e7a74ea45d4f959278e
Carlos Nikiforuk <CarNikiforuk@gmail.com>
2015-01-22 09:01:32 -0700

    done

--- ass1/main.py
+++ ass1/main.py
@@ -19,77 +19,44 @@ def main():
     if len(sys.argv) == 1 or sys.argv[1] in {"-h", "--help"}:
         print("usage: {0} <db file>".format(sys.argv[0]))
         exit()
-    
     infile = sys.argv[1]
+    
+    try:
+        open(infile,'r')
+    except FileNotFoundError:
+        print("Input file does not exist.")
+        ans = input("Would you like to create it as a new database? (y/n)? ")
+        if(ans == 'y'):
+            open(infile, 'x')
+        else:
+            exit()
+    except Exception as e:
+        print("Error: ",e)
+            
 
     db1 = db.database(infile)
     cont = True
     
     while(cont):
     
-        print("\n-----------Employee FMS-----------\n")
-        print("Select one of the following:\n")
-        print("    1) Add a new employee")
-        print("    2) Search for an employee")   
-        print("    3) Remove an employee from FMS")
-        print("    4) Display the entire employee FMS")
-        print("    5) Quit")
+        choice = db.displayMenu()
 
-        a = input("\nOption? ")
-        while(a < '1' or a > '5'):
-            print("Invalid input")
-            a = input("\nOption? ")
+        if  (choice == '1'):   #add employee
+            db1.addEmployee()
 
-        if  (a=='1'):   #add employee
-            line = input("Enter employee string to add: ") #xxxx:first:last:dept
-            line = line.rstrip()
-            fields = line.split(":")
-            f = db1.findEmployee(fields[ID])
-            try:
-                if(MAXNAMESIZE < max([len(fields[FIRST]), len(fields[LAST]), len(fields[DEPT])])):
-                    print("Name or dept too long!")
-                elif(f == 0 and len(fields[ID]) == 4):
-                    db1.addEmployee(line)
-                    print("Employee ",fields[ID]," Added!")
-                else:
-                    print("Error: Bad ID or duplicate entry")
-            except IndexError:
-                print("Error: Incomplete string!")
-            except Exception as e:
-                print("Error: ",e)
+        elif(choice == '2'):   #search for employee
+            db1.findEmployee()
 
-        elif(a=='2'):   #search for employee
-            id = input("Enter employee ID to find: ")
-            f = db1.findEmployee(id)
-            if(f == 0):
-                print("\nNot found!")
-            else:
-                print("{0:<4} {1:<{n}} {2:<15}".format("ID", "Name", "Dept", n=MAXNAMESIZE*2))
-                print("{0:-<4} {0:-<{n}} {0:-<15}".format("", n=MAXNAMESIZE*2))
-                db1.printEmployee(f)
-            
-        elif(a=='3'):   #remove employee
-            id = input("\nEnter employee ID to remove: ")
-            f = db1.findEmployee(id)
-            if(f == 0):
-                print("\nEmployee not found!")
-            else:
-                print("Employee found: ")
-                print("{0:<4} {1:<{n}} {2:<15}".format("ID", "Name", "Dept", n=MAXNAMESIZE*2))
-                print("{0:-<4} {0:-<{n}} {0:-<15}".format("", n=MAXNAMESIZE*2))
-                db1.printEmployee(f)
-                verify = input("\nAre you sure you with to delete this employee (y/n)? " )
-                if(verify == 'y'):
-                    db1.removeEmployee(f)
-                    print("\nEmployee ",id," removed!")
+        elif(choice == '3'):   #remove employee
+            db1.removeEmployee()
 
-        elif(a=='4'):   #show employees
+        elif(choice == '4'):   #show employees
             db1.showEmployees()
 
-        elif(a=='5'):    #quit
+        elif(choice == '5'):   #quit
             cont = False
         
-        if(a != '5'):
+        if(choice != '5'):
             input("\nPress enter to continue...")
 
 
