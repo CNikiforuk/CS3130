9f849199675e06a2e5dcd742d008e78da5c58ea2
CNikiforuk <CarNikiforuk@gmail.com>
2015-02-12 13:08:23 -0700

    ass3 done

--- ass3/db.py
+++ ass3/db.py
@@ -1,8 +1,17 @@
+################--Description--#################
+#Database Module
+
+################-----Author-----#################
+#Carlos Nikiforuk
+
 import argparse, random, socket, sys, getpass
 
 MAX_BYTES = 65535
 
+##################################
 class database:
+#Database class containing file handle and user dict.
+##################################
 
     def __init__(self, infile):
         self.infile = infile
@@ -13,32 +22,41 @@ class database:
             if line:
                 name, pw = line.split(':',1)
                 self.users[name] = [(), pw, 0]
-                
+    ##################################    
     def loginUser(self, name, pw, address):
+    #login user, returns 1 if already logged in, 0 if success, -1 if fail
+    ##################################
         if (name in self.users):
-            if(self.users[name][1] == pw):
+            if(self.users[name][1]==pw):
                 if(self.users[name][2]==1):
                     return 1
                 else:
                     self.users[name][0] = address
                     self.users[name][2] = 1
-                    print("User "+name+" logged in:")
-                    print(self.users[name][0])
                     return 0
             
         return -1
-        
+
+    ##################################
     def signOut(self, name):
+    #signout user, no return
+    ##################################
         self.users[name][2] = 0
-        
+    
+    ##################################   
     def whoison(self):
+    #return who is online as a string
+    ##################################
         online = ''
         for k in self.users.keys():
             if(self.users[k][2] == 1):
                 online = online+k+'\n>>'
         return online
-        
+    
+    ##################################
     def getUser(self, address):
+    #Address passed in, user name returned
+    ##################################
         for k in self.users.keys():
             if(self.users[k][0] == address):
                 return k
@@ -46,12 +64,6 @@ class database:
 class user:
     def __init__(self, name, pw):
         self.info = [name,pw,0]
-
-
-    
-
-#def dbOpen(infile):
-#    with open(self.infile, 'r') as file:
        
     
 def main():
--- ass3/main.py
+++ ass3/main.py
@@ -1,28 +1,141 @@
 #!/usr/bin/env python3
 
-#make message protocol to specify login, message, special, source user, etc.
-#implement threads
+################--Description--#################
+#Main Module
+#Python client server messaging system
+
+###############-----Author-----#################
+#Carlos Nikiforuk
 
 import argparse, random, socket, sys, getpass, db, threading, re
 from datetime import datetime
 
 MAX_BYTES = 65535
 
+
+##################################
 class message:
+#Message class, contains message type and text
+##################################
 
     def __init__(self, type, text):
         self.type = type
-        #self.time
+        #self.time = datetime.now().strftime('%b %d %Y %H:%M:%S')
         self.text = text
-        
+    ##################################
     def toString(self):
+    #Converts message to its string representation
+    ##################################    
         return self.type+':'+self.text
-        
+
+##################################       
 def toMessage(text):
+#Returns message object of passed in text
+##################################
         fields = text.split(':',1)
         return message(fields[0], fields[1])
+    
+################################## 
+def server(interface, port):                                        #SERVER
+#Main Server
+##################################
+
+    userDB = db.database('users')
+    helpText = "The following commands are supported:\n\nwhoIsOn\nsend <user> <message>\nsignout"
+
+    try:    
+        logFile = open('server.log', 'r+')
+    except FileNotFoundError:
+        logFile = open('server.log', 'w+')
+    logFile.seek(0,2)       #seek to end of file.
+
+    msgFiles = {}
+    for k in userDB.users.keys():
+        try:
+            msgFiles[k] = open('messages/'+k, 'r+')
+        except FileNotFoundError:
+            msgFiles[k] = open('messages/'+k, 'w+')
+            
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((interface, port))
+    print('Listening at', sock.getsockname())
+    
+    user = ''
+
+    while True:
+        inData, address = sock.recvfrom(MAX_BYTES)
+        inMsg = toMessage(inData.decode('ascii'))
         
+        if(inMsg.type == '0'):
+            serverOut = "{}: Login attempt by: {}".format(datetime.now().strftime('%b %d %Y %H:%M:%S'),repr(address))
+            logFile.write(serverOut+'\n')
+            logFile.flush()
+            print(serverOut)
+            name, pw = inMsg.text.split(':',1)
+            attempt = userDB.loginUser(name, pw, address)
+            if(attempt == 0):
+                serverOut = "{}: {} logged in. Address:  {}".format(datetime.now().strftime('%b %d %Y %H:%M:%S'), name, repr(address))
+                logFile.write(serverOut+'\n')
+                logFile.flush()
+                print(serverOut)
+                msgFiles[name].seek(0)
+                msg = message('0',"Login Successful!\n\n-----Welcome to MMS!-----\n\n"+helpText+"\n\n")
+                for line in msgFiles[name]:
+                    msg.text = msg.text + '>>' + line +'\n'
+                print(msgFiles[name].read())
+                msgFiles[name].seek(0)
+                msgFiles[name].truncate()
+                user = userDB.getUser(address)
+                #THREAD DELEGATION MAYBE
+                
+            elif(attempt == -1):
+                msg = message('-1',"Inocorrect username or password!")
+            else:
+                msg = message('-1',"User is already logged in!")
+            
+        elif(inMsg.type == '1'):
+            serverOut = "{}: {}: {}".format(datetime.now().strftime('%b %d %Y %H:%M:%S'), user, inMsg.text)
+            logFile.write(serverOut+'\n')
+            logFile.flush()
+            print(serverOut)
+            tokens = inMsg.text.lstrip().split(' ',2)
+            if(tokens[0] == 'send'):
+                if(len(tokens) >= 3):
+                    msg = message('1', 'sent!')
+                    outMsg = message('1', user+': '+tokens[2])
+                    outData = outMsg.toString().encode('ascii')
+                    if tokens[1] in userDB.users.keys():
+                        if(userDB.users[tokens[1]][2] == 1):
+                            print("Sending Message to "+tokens[1])
+                            sock.sendto(outData, userDB.users[tokens[1]][0])
+                        else:
+                            msg = message('1', 'User is offline, message will be sent when they signin.')
+                            msgFiles[tokens[1]].write(outMsg.text+'\n')
+                            msgFiles[tokens[1]].flush()
+                    else:
+                        msg = message('1', 'User doesnt exist!')
+                else:
+                    msg = message('1', 'Incorrect send format!')
+
+            elif(tokens[0] == 'signout'):            
+                msg = message('5', 'Thank you for using MMS!')
+                userDB.signOut(user)
+                #signout
+            elif(tokens[0] == 'whoison'):
+                msg = message('1',userDB.whoison())
+            else:
+                msg = message('1', helpText)
+        
+        #print("Sending out: '" + outMsg.text +"'")
+        data = msg.toString().encode('ascii')
+        sock.sendto(data, address)
+
+
+################################## 
 def loginPrompt(sock, delay):
+#Prompts user to login, returns login message from server
+##################################
     print("\n-----Signin to MMS!-----\n")
     user = input('Enter username: ')
     password = getpass.getpass()
@@ -45,84 +158,11 @@ def loginPrompt(sock, delay):
             break
             
     return(inMsg)
-    
-    
-    
-def server(interface, port):                                        #SERVER
-    userDB = db.database('users')
-    helpText = "The following commands are supported:\n\nwhoIsOn\nsend <user> <message>\nsignout"
-    
-    msgFiles = {}
-    for k in userDB.users.keys():
-        try:
-            msgFiles[k] = open('messages/'+k, 'r+')
-        except FileNotFoundError:
-            msgFiles[k] = open('messages/'+k, 'w+')
-            
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((interface, port))
-    print('Listening at', sock.getsockname())
 
-    while True:
-        inData, address = sock.recvfrom(MAX_BYTES)
-        inMsg = toMessage(inData.decode('ascii'))
-        
-        if(inMsg.type == '0'):
-            print("Attempt login!") #check database
-            name, pw = inMsg.text.split(':',1)
-            attempt = userDB.loginUser(name, pw, address)
-            if(attempt == 0):
-                msgFiles[name].seek(0)
-                msg = message('0',"Login Successful!\n\n-----Welcome to MMS!-----\n\n"+helpText+"\n\n")
-                for line in msgFiles[name]:
-                    msg.text = msg.text + '>>' + line +'\n'
-                print(msgFiles[name].read())
-                msgFiles[name].seek(0)
-                msgFiles[name].truncate()
-                user = userDB.getUser(address)
-                #THREAD DELEGATION MAYBE
-                
-            elif(attempt == -1):
-                msg = message('-1',"Inocorrect username or password!")
-            else:
-                msg = message('-1',"User is already logged in!")
-            
-        elif(inMsg.type == '1'):
-            tokens = inMsg.text.lstrip().split(' ',2)
-            print(tokens)
-            if(tokens[0] == 'send'):
-                if(len(tokens) >= 3):
-                    msg = message('1', 'sent!')
-                    outMsg = message('1', user+': '+tokens[2])
-                    outData = outMsg.toString().encode('ascii')
-                    if tokens[1] in userDB.users.keys():
-                        if(userDB.users[tokens[1]][2] == 1):
-                            print("ATTEMPTING SEND")
-                            sock.sendto(outData, userDB.users[tokens[1]][0])
-                        else:
-                            msg = message('1', 'User is offline, message will be sent when they signin.')
-                            msgFiles[tokens[1]].write(outMsg.text+'\n')
-                            msgFiles[tokens[1]].flush()
-                    else:
-                        msg = message('1', 'User doesnt exist!')
-                else:
-                    msg = message('1', 'Incorrect send format!')
-
-            elif(tokens[0] == 'signout'):
-                msg = message('5', 'signout!')
-                userDB.signOut(user)
-                #signout
-            elif(tokens[0] == 'whoison'):
-                msg = message('1',userDB.whoison())
-            else:
-                msg = message('1', helpText)
-        
-        #print("Sending out: '" + outMsg.text +"'")
-        data = msg.toString().encode('ascii')
-        sock.sendto(data, address)
-        
+################################## 
 def receiver(sock):
+#Client thread for receiving messages from server
+################################## 
     while True:
         sock.setblocking(True)
         data = sock.recv(MAX_BYTES)
@@ -132,10 +172,12 @@ def receiver(sock):
             sys.exit(0)
         print('>',end='',flush=True)
             
+################################## 
 def sender(sock):
+#Client thread for sending messages to server
+################################## 
      while True:
         delay = 0.1  # seconds
-        #t = "{}: ".format(datetime.now().strftime('%b %d %Y %H:%M:%S')); #nice format
         text = input('>')
         msg = message('1',text)
         text = msg.toString()
@@ -143,8 +185,11 @@ def sender(sock):
         sock.send(data)
         if(msg.text == 'signout'):
             sys.exit(0)
-            
-def client(hostname, port):                                         #CLIENT
+
+##################################             
+def client(hostname, port):                                        #CLIENT
+#Main Client
+################################## 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     hostname = sys.argv[2]
     sock.connect((hostname, port))
