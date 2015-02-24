ca1595a230431296cb1c7a630d20ec84fad696b7
CNikiforuk <CarNikiforuk@gmail.com>
2015-02-24 10:33:20 -0700

    added loop to test multiple phone numbers

--- lab2/RE.py
+++ lab2/RE.py
@@ -7,18 +7,21 @@ import re
 #Carlos Nikiforuk
 
 def main():
-    good = False
-    new = ""     
-    alpha = False           
-        
-    inp = input("Enter a phone number: ")
+    print("\n-----Phone Number formatting----\nTo quit, type quit or exit.\n")
+    while(True):
+        good = False
+        new = ""     
+        alpha = False           
+        inp = input("Enter a phone number: ")
+        if(inp.lower() not in ['quit', 'exit']):
+            format = re.match("([\d]{10})|(\([\d]{3}\)( |)[\d]{3}( |-)[\d]{4})|([\d]{3}(-| )[\d]{3}(-| )[\d]{4})",inp)
+            if(format):
+                num = re.sub('\D+','',inp)
+                print("({}) {}-{}".format(num[0:3],num[3:6],num[6:10]))
+            else:
+                print("Bad Phone Number")
+        else:
+            exit()
     
-    format = re.match("([\d]{10})|(\([\d]{3}\)( |)[\d]{3}( |-)[\d]{4})|([\d]{3}(-| )[\d]{3}(-| )[\d]{4})",inp)
-    if(format):
-        num = re.sub('\D+','',inp)
-        print("({}) {}-{}".format(num[0:3],num[3:6],num[6:10]))
-    else:
-        print("Bad Phone Number")
-        
 
 main()
