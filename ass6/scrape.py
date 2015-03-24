831d5f15e08699d938a2f254d88caef54de6895e
CNikiforuk <CarNikiforuk@gmail.com>
2015-03-24 11:06:08 -0600

    ass6 final

--- ass6/scrape.py
+++ ass6/scrape.py
@@ -1,37 +1,48 @@
-import requests, re, urllib
+import requests, re
+#Author: Carlos Nikiforuk
+#Description: Python script for retrieving market info from mail and globe.
+#Date: March 26, 2015
 
-#TODO Format Output. Update before printing. Ask to continue. error checking
+#TODO HMM
 
 def main():
 
-
+    url = "http://www.theglobeandmail.com/globe-investor/"
+    
+    #connect to url
+    r = requests.get(url)
+    while(r.status_code != 200):
+        print("Failed to connect to Globe and Mail. Press any key to try again.")
+        input()
+        r = requests.get(url)
+        
     #get the relevant HTML section
-    r = requests.get("http://www.theglobeandmail.com/globe-investor/")
     a = r.text.split('MiniMarketsDashboardModule')
     b = a[2].split('\n')
     string = b[0]
     
-    #get names
+    #get names and filter empty lines
     names = re.findall('(class="positive">(.*?)<)|(class="negative">(.*?)<)|(<th>(.*?)</th>)', string)
+    filterRE(names)   
     
-    #Request input
+    #request input
     print("\n-----------Market Information-----------\n")
     print("Select one of the following:\n")
  
     for i in range(0, len(names)):
         print("    {}) {}".format(i+1, names[i][1]))
         
-    inp = input('Option? ')
-    key = names[int(inp)-1][1]
+    inp = input('\nOption? ')
+    while(inp < '1' or ord(inp) > len(names)+48):
+        inp = input('\nOption? ')
     
     
-    #parse through it
+    #parse information
     values = re.findall('(\$</span>(.*?)<)|(chars9">(.*?)<)|(chars8">(.*?)<)', string)
     changes = re.findall('class="(neg color ">(.*?)</span>)|(pos color ">(.*?)</span>)', string)
     times = re.findall('class="timing update-info" data=" (EDT|CDT)">(.*?)</span>', string)
     
     #filter out empty lines
-    filterRE(names)    
     filterRE(values)
     filterRE(changes)
     filterRE(times)
@@ -45,11 +56,17 @@ def main():
         data[names[i][1]].append(changes[i*2+1][1])
         data[names[i][1]].append(times[i][1])
         
+    #format and output
+    key = names[int(inp)-1][1]
+    print("\n{0:<{m}}{1:<{m}}{2:<{m}}{3:<{m}}".format('Name', 'Value', 'Change', 'Time', m=15))
+    print("{0:-<{m}}".format("", m=15*4))
+    print("{0:<{m}}{1:<{m}}{2:<{m}}{3:<{m}}\n".format(key, '$'+data[key][0], data[key][1]+' '+data[key][2], data[key][3], m=15))
     
-    print("\n    {}\n    {}\n    {} {}\n    {}\n".format(key, data[key][0], data[key][1], data[key][2], data[key][3]))
+    
     
 
 def filterRE(m):
+#remove empty lines
     if m:    
         for i in range(0,len(m)):
             m[i] = list(filter(None, m[i]))
@@ -59,6 +76,7 @@ def filterRE(m):
         return 0
         
 def printAll(data):
+#output all data gathered
     for key in data.keys():
         print("{}: {} {} {} {}".format(key, data[key][0], data[key][1], data[key][2], data[key][3]))
 
